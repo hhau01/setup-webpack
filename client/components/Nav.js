a3cd72fecb7dc99fc1d4048cad38096ed282de94
Andrew Harris <ididrio@gmail.com>
2018-01-29 13:53:44 -0800

    added components

---
+++ client/components/Nav.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Nav extends Component {
+  render() {
+    return (
+      <div id='nav'>
+        Nav
+      </div>
+    );
+  }
+}
+
+export default Nav;
