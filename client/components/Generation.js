fc3eb965ac2bb0d94fc32a9acc4afaeaa3d1220a
Andrew Harris <ididrio@gmail.com>
2018-01-30 11:05:59 -0800

    modified generation to handle multiple filepaths

--- client/components/Generation.js
+++ client/components/Generation.js
@@ -2,9 +2,18 @@ import React, { Component } from 'react';
 import { get } from 'react-agent';
 
 class Generation extends Component {
+
+  componentDidMount() {
+    const script = document.createElement('script');
+    script.src = 'https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js';
+    script.async = true;
+    document.body.appendChild(script);
+  }
+
   render() {
     let entries = get('entry').split('/').map((e, i, a) => {
-      if (i !== a.length - 1) return e + `'`;
+      if (i !== 0 && i !== a.length - 1) return `'` + e + `',`;
+      else if (i === 0 && a.length !== 1) return e + `',`;
       else if (a.length !== 1) return `'` + e;
       else return e;
     }).join(' ');
@@ -12,15 +21,16 @@ class Generation extends Component {
     let outputs = get('output').split('/');
     const filename = outputs.pop();
     outputs = outputs.map((e, i, a) => {
-      if (i !== outputs.length - 1) return e + `'`;
-      else if (a.length !== 1) return `'` + e;
+      if (i !== 0 && i !== outputs.length - 1) return `'` + e + `',`;
+      else if (i === 0 && outputs.length !== 1) return e + `',`;
+      else if (outputs.length !== 1) return `'` + e;
       else return e;
     }).join(' ');
 
     return (
       <div id='generation'>
         <h2>Your Webpack Config</h2>
-        <pre>
+        <pre className='prettyprint lang-js'>
           {`const path = require('path');`}<br /><br />
           {`module.exports = {`}<br />
           {`  entry: path.resolve(__dirname, '${entries}'),`}<br />
