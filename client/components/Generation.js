d7ab1bc6bcd72bf5ab11c6522de9fbd3be673ecb
Aaron Elkins <aaronelkins@Aarons-MacBook-Pro.local>
2018-01-31 14:29:37 -0800

    Plugin merged with Libs and Loaders

--- client/components/Generation.js
+++ client/components/Generation.js
@@ -6,7 +6,8 @@ class Generation extends Component {
   renderNpm() {
     const loaders = get('loaders');
     const plugins = get('plugins');
-    if (loaders.length > 0) {
+    const libraries = get('libraries');
+    if (loaders.length + libraries.length + plugins.length > 0) {
       let string = 'npm install --save-dev';
       loaders.forEach(loader => {
         if (loader.includes('babel') && !string.includes(' babel-loader babel-core')) {
@@ -15,11 +16,12 @@ class Generation extends Component {
         string += ` ${loader}`
       })
       plugins.forEach(plugins => {
-        if (plugins.includes('babel') && !string.includes(' babel-loader babel-core')) {
-          string += ' babel-loader babel-core';
-        }
         string += ` ${plugins}`
       })
+      libraries.forEach(library => {
+        string += ` ${library}`	
+      });
+
       return (
         <div>
           <h2>npm install</h2>
@@ -28,6 +30,83 @@ class Generation extends Component {
       );
     }
   }
+  
+  renderResolve() {		
+    const loaders = get('loaders');		
+    let babel = false, ts = false;		
+    loaders.forEach(loader => {		
+      if (loader.includes('babel')) babel = true;		
+    else if (loader === 'ts-loader')  ts = true;		
+    });		
+    let extensions = ['.js'];		
+    if (babel) extensions = extensions.concat(['.jsx']);		
+    if (ts) extensions = extensions.concat(['.ts', '.tsx']);		
+      extensions = extensions.map(e => `'${e}'`);		
+      if (babel || ts) {		
+        return (		
+    `  resolve: {		
+      extensions: [${extensions.join(', ')}]		
+    },		
+  `);		
+      }		
+    }		
+  		
+    renderModules() {		
+      const loaders = get('loaders');		
+      if (loaders.length > 0 ) {		
+        return (		
+  `  modules: {		
+      rules: [		
+        ${renderLoaders()}		
+      ]		
+    },		
+  `);		
+      }		
+       function renderLoaders() {		
+         let babel = false;		
+         loaders.forEach(loader => {		
+           if (loader.includes('babel')) babel = true;		
+         });		
+         let singleBabel = [...loaders];		
+         if (babel) {		
+           singleBabel = singleBabel.filter(l => !l.includes('babel'));		
+           singleBabel.unshift('babel');		
+         }		
+        return singleBabel.map((loader, i) => {		
+          if (loader === 'babel') {		
+            return (		
+          `{		
+          test: /\.jsx?/,		
+          exclude: /(node_modules|bower_components)/,		
+           use: {		
+            loader: 'babel-loader',		
+            options: {		
+              presets: ['@babel/${loaders.filter(l => l.includes('babel')).join('/')}']		
+            }		
+          }		
+        },${singleBabel.length - 1 !== i ? '\n' : ''}`);		
+          } else if (loader === 'json-loader') {		
+            return (		
+          `${i === 0 ? '{' : '      {'}		
+          test: /\.json$/,		
+          loader: 'json-loader'		
+        },${singleBabel.length - 1 !== i ? '\n' : ''}`);		
+          } else if (loader === 'svg-url-loader') {		
+            return (		
+          `${i === 0 ? '{' : '      {'}		
+          test: /\.svg/,		
+          loader: 'svg-url-loader'		
+        },${singleBabel.length - 1 !== i ? '\n' : ''}`);		
+          } else if (loader === 'ts-loader') {		
+            return (		
+          `${i === 0 ? '{' : '      {'}		
+          test: /\.tsx?$/,		
+          loader: 'ts-loader'		
+        },${singleBabel.length - 1 !== i ? '\n' : ''}`);		
+          }		
+         }).join('');		
+      }		
+     }
 
   render() {
     let entries = get('entry').split('/').filter(e => e !== '.' && e.length > 0)
@@ -55,6 +134,8 @@ class Generation extends Component {
           {`module.exports = {`}<br />
           {`  entry: path.resolve(__dirname, '${entries}'),`}<br />
           {`  output: {`}<br />
+          {this.renderModules()}		
+          {this.renderResolve()}
           {`    path: path.resolve(__dirname, '${outputs}'),`}<br />
           {`    filename: '${filename}'`}<br />
           {`  },`}<br />
