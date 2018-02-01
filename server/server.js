4aca77759320b255bd994de83a484b9c84e81b19
Andrew Harris <ididrio@gmail.com>
2018-01-31 16:40:09 -0800

    added save button

--- server/server.js
+++ server/server.js
@@ -11,18 +11,24 @@ const SERVER_PORT = process.env.SERVER_PORT || 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// const sequelize = new Sequelize(keys.POSTGRES_DBNAME, keys.POSTGRES_USERNAME, keys.POSTGRES_PASSWORD, {
-//   dialect: 'postgres',
-//   host: keys.POSTGRES_URI,
-//   port: keys.POSTGRES_PORT,
-//   operatorsAliases: Sequelize.Op,
-//   logging: false,
-// });
-// sequelize.authenticate()
-//   .then(() => console.log('Connected to Postgres DB...'))
-//   .catch(err => console.error('Unable to connect to DB...', err));
+const sequelize = new Sequelize(keys.POSTGRES_DBNAME, keys.POSTGRES_USERNAME, keys.POSTGRES_PASSWORD, {
+  dialect: 'postgres',
+  host: keys.POSTGRES_URI,
+  port: keys.POSTGRES_PORT,
+  operatorsAliases: Sequelize.Op,
+  logging: false
+});
+sequelize.authenticate()
+  .then(() => console.log('Connected to Postgres DB...'))
+  .catch(err => console.error('Unable to connect to DB...', err));
 
 app.use(express.static(path.join(__dirname, '../build')));
+
+app.post('/configs', (req, res) => {
+  console.log(req.body);
+  res.json({ ok: 'ok' });
+});
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
